0b833709c8c8af51daaa868fee21a304f92757c2
webcatz <june.webcatz@gmail.com>
2024-06-24 21:35:50 +0400

    added console

--- script/game.gd
+++ script/game.gd
@@ -58,3 +58,27 @@ func _ready() -> void:
 	data = load("res://resource/save.tres")
 	
 	add_child(_pause_menu)
+
+
+
+# debug
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and event.keycode == KEY_P and event.shift_pressed:
+		var layer: CanvasLayer = CanvasLayer.new()
+		var input: LineEdit = LineEdit.new()
+		input.anchor_right = Control.ANCHOR_END
+		layer.add_child(input)
+		get_root().add_child(layer)
+		input.grab_focus()
+		
+		await input.text_submitted
+		
+		var command: String = input.text
+		layer.queue_free()
+		
+		if command:
+			var script: GDScript = GDScript.new()
+			script.source_code = "static func run():" + command
+			script.reload()
+			script.run()
