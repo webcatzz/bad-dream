2967329f630922513b296ace6624159151019c1a
webcatzz <june.webcatz@gmail.com>
2024-08-12 22:36:46 +0400

    font tinkering

--- script/game.gd
+++ script/game.gd
@@ -11,3 +11,13 @@ var party_node: Node2D
 func set_place(key: String) -> void:
 	current_place = key
 	get_tree().change_scene_to_file("res://place/%s.tscn" % key)
+
+
+
+# input
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if Input.is_action_pressed("screenshot"):
+		get_viewport().get_texture().get_image().save_png("user://screenshot_%s.png" % Time.get_datetime_string_from_system())
+		print("took screenshot!")
+		get_viewport().set_input_as_handled()
