1b5cf0c3bdba4a8fd1ac5c22a7cefb1ad3fd696e
webcatzz <june.webcatz@gmail.com>
2024-12-24 13:10:21 +0400

    gate system awaits different signal

--- autoload/game/game.gd
+++ autoload/game/game.gd
@@ -10,6 +10,5 @@ var grid := IsoGrid.new()
 
 func change_scene(filename: String, target_gate: String = "") -> void:
 	get_tree().change_scene_to_file("res://place/%s.tscn" % filename)
-	await get_tree().process_frame
-	await get_tree().process_frame
+	await get_tree().tree_changed
 	if target_gate: get_tree().call_group("gate", "receive", target_gate)
